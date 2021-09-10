76af988dc2d75819f458dd9444c2cef697e6fbf2
John Teckemeyer <johnteckemeyer@gmail.com>
2021-09-09 20:29:03 -0700

    Add a reusable Paragraph component and use for about me info and wrap it and the profile image in a flex div that toggles direction dependent upon width of screen

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Banner from '../../Components/Banner/Banner';
 import ContentContainer from '../../Components/ContentContainer/ContentContainer';
 import ProfileImage from '../../Components/ProfileImage/ProfileImage';
+import Paragraph from '../../Components/Paragraph/Paragraph';
 import './HomePage.scss';
 
 export default function HomePage (props) {
@@ -10,7 +11,12 @@ export default function HomePage (props) {
         <div classname="homepage-container">
             <Banner />
             <ContentContainer>
-                <ProfileImage />
+                <div className="profile-wrapper">
+                    <ProfileImage />
+                    <Paragraph>
+                        I AM A FULL-STACK WEB DEVELOPER OUT OF THE SAN FRANCISCO BAY AREA - DETERMINED TO BRING UNIQUE EXPERIENCES FOR USERS THROUGH OUT-OF-THE-BOX SOLUTIONS. WITH A BACKGROUND IN MUSIC COMPOSITION AND HANDCRAFTING CLASSICAL GUITARS, I STRIVE TO IMPLEMENT AS MUCH ARTISTRY IN MY PROJECTS AS I CAN.
+                    </Paragraph>
+                </div>
             </ContentContainer>
         </div>
     )
