878fd98590eb3bbba5bbc1b65f11653a944a8b6f
John Teckemeyer <johnteckemeyer@gmail.com>
2021-09-11 15:16:33 -0700

    Refactor nav so links can be internal as well as external

---
+++ src/Components/NavLink/NavLink.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import './NavLink.scss';
+
+export default function NavLink (props) {
+
+    const history = useHistory();
+
+    function handleLinkClick () {
+        if (props.link === "/home")
+            history.push(props.link);
+    }
+
+    return (
+        <>
+            {
+                props.link === "/home" ? 
+                    <div className="nav-back" onClick={handleLinkClick}>
+                        <div className="material-icons custom-icon">{props.icon}</div>
+                        <div className="link-text">{props.text}</div>
+                    </div>
+                :
+                    <a  className="nav-link" href={props.link} rel="noreferrer" target="_blank">
+                        <div className="material-icons custom-icon">{props.icon}</div>
+                        <div className="link-text">{props.text}</div>
+                    </a>
+            }
+        </>
+    )
+}
