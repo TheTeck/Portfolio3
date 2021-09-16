03680d3d64c43ed9abf26dc919a860efd3783211
John Teckemeyer <johnteckemeyer@gmail.com>
2021-09-16 15:27:44 -0700

    Restyle pages and refactor the project sections style for focus

--- src/Pages/PuzzlePage/PuzzlePage.jsx
+++ src/Pages/PuzzlePage/PuzzlePage.jsx
@@ -3,7 +3,9 @@ import React, { useState } from 'react';
 import ContentContainer from '../../Components/ContentContainer/ContentContainer';
 import ProjectNav from '../../Components/ProjectNav/ProjectNav';
 import Banner from '../../Components/Banner/Banner';
+import Paragraph from '../../Components/Paragraph/Paragraph';
 import ProjectSection from '../../Components/ProjectSection/ProjectSection';
+import TechList from '../../Components/TechList/TechList';
 import './PuzzlePage.scss';
 
 export default function PuzzlePage (props) {
@@ -14,6 +16,11 @@ export default function PuzzlePage (props) {
         site: "https://just-puzzles.herokuapp.com/",
         github: "https://github.com/TheTeck/JustPuzzles"
     }
+
+    const tech = ['HTML', 'SCSS', 'Javascript', 'React', 'Node', 'MongoDB', 'AWS S3'];
+
+    const description = <Paragraph><strong>Just Puzzles</strong> is an online source for puzzles that takes an image and breaks it up into the number of pieces and piece shape specified by the user. As the admin, I can upload many images ahead of time and have them released upon the date given to each image for a hands-off daily update. The puzzle mechanics and image selections were designed to create a enjoyable, relaxing and satisfying user experience.</Paragraph>
+    const objective = <Paragraph>Why I made this...</Paragraph>
 
     function handleActivateSection (section) {
         if (activeSection === section) {
@@ -29,9 +36,13 @@ export default function PuzzlePage (props) {
             <ContentContainer>
                 <ProjectNav links={links} />
                 <ProjectSection active={activeSection} code="des" title="Description" handleActivateSection={handleActivateSection}>
-                    <div>This is a test fjdowiejf owiejf lksjf owi ejf kjsowie jflks odkfjlskdjfowijef s owee dow we </div>
+                    {description}
+                    <TechList tech={tech} />
                 </ProjectSection>
-                <ProjectSection active={activeSection} code="obj" title="Objective" handleActivateSection={handleActivateSection} />
+                <ProjectSection active={activeSection} code="obj" title="Objective" handleActivateSection={handleActivateSection}>
+                    {objective}
+                </ProjectSection>
+                <ProjectSection active={activeSection} code="img" title="Images" handleActivateSection={handleActivateSection} />
                 <ProjectSection active={activeSection} code="cod" title="Code Highlights" handleActivateSection={handleActivateSection} />                
             </ContentContainer>
         </div>
