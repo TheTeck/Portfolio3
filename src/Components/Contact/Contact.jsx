1499886c9a67e98ca89ce559c8b374e2087c88d7
John Teckemeyer <johnteckemeyer@gmail.com>
2021-09-20 22:15:07 -0700

    Add flex containers in the contact component to move the content from a column to a row, dependent upon screen size

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,9 +4,16 @@ import './Contact.scss';
 
 export default function Contact (props) {
     return (
-        <a className="resume-container" href="resume.pdf" target="_blank">
-            <div className="material-icons resume-icon">description</div>
-            <div className="resume-text">Resume</div>
-        </a>
+        <div className="contact-container">
+            <div className="resume-container">
+                <a href="resume.pdf" target="_blank">
+                    <div className="material-icons resume-icon">description</div>
+                    <div className="resume-text">Resume</div>
+                </a>
+            </div>
+            <div className="contact-info-container">
+
+            </div>
+        </div>
     )
 }
